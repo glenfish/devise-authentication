536e9990ec0ada60c9b131bbd98ae0f84a4a92ec
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-02-05 16:21:39 +1100

    added page protection for non-authenticated uasers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:restricted]
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/pages/restricted", to: "pages#restricted"
+  get "/restricted", to: "pages#restricted"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
